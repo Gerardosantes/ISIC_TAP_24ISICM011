3e593a7c0928d778010f33386f87e3c201b6bb1e
Gerardosantes <perezzg816@gmail.com>
2026-03-17 18:49:20 -0600

    le agreme el metodo scanner que pida la informacion por medio del teclado y un encabezado

--- unidad1/ejercicio4/src/principal/Main.java
+++ unidad1/ejercicio4/src/principal/Main.java
@@ -1,16 +1,35 @@
 
 package principal;
 
+import java.util.Scanner;
+
 
 public class Main {
     
     public static void main(String[] args) {
         
         
+        Scanner sc = new Scanner(System.in);
+        
      int m=16;
      int a=5;
      int c=3;
      int x=4;
+     
+     System.out.print("Ingresa el valor de m: ");
+         m = sc.nextInt();
+        
+        System.out.print("Ingresa el valor de a: ");
+        a = sc.nextInt();
+        
+        System.out.print("Ingresa el valor de c: ");
+        c = sc.nextInt();
+        
+        System.out.print("Ingresa el valor inicial x: ");
+         x = sc.nextInt();
+    
+         
+         System.out.println("Iteración\tX\tU");
      
         for (int i = 0; i < 5; i++) {
             x=(a*x+c)%m;
@@ -20,6 +39,7 @@ public class Main {
             System.out.println(i+"\t\t"+x+"\t"+u);
         }
         
+          sc.close();
     }
     
 }
